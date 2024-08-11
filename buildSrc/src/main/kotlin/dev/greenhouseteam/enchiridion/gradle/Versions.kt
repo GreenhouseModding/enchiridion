10b31b829a8923cd9505a8e7f6faf99d3dc06e8a
Pug <81167232+MerchantPug@users.noreply.github.com>
2024-08-11 15:24:13 +1000

    Update Versions.kt

--- buildSrc/src/main/kotlin/dev/greenhouseteam/enchiridion/gradle/Versions.kt
+++ buildSrc/src/main/kotlin/dev/greenhouseteam/enchiridion/gradle/Versions.kt
@@ -9,18 +9,18 @@ object Versions {
     const val PARCHMENT = "2024.07.07"
 
 
-    const val FABRIC_API = "0.100.8+1.21"
+    const val FABRIC_API = "0.102.0+1.21.1"
     const val FABRIC_LOADER = "0.15.11"
     const val MOD_MENU = "11.0.1"
     const val FABRIC_LOADER_RANGE = ">=0.15"
     const val FABRIC_MINECRAFT_RANGE = ">=1.21"
 
-    const val NEOFORGE = "21.0.148"
+    const val NEOFORGE = "21.1.4"
     const val NEOFORGE_LOADER_RANGE = "[4,)"
     const val NEOFORGE_MINECRAFT_RANGE = "[1.21,)"
 
-    const val FABRIC_MIXIN = "0.14.0+mixin.0.8.6"
-    const val MIXIN_EXTRAS = "0.3.6"
+    const val FABRIC_MIXIN = "0.15.0+mixin.0.8.7"
+    const val MIXIN_EXTRAS = "0.4.0"
 
     const val JAVA = 21
 }
